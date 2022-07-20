843652a12ebb0e611c26675b65d05e784caa5bdc
Harshit Pranu Srivastava <108911685+Harshitpranu@users.noreply.github.com>
2022-07-20 13:43:38 +0530

    Add files via upload
    comments

--- gussing_game.py
+++ gussing_game.py
@@ -1,14 +1,15 @@
-import random
-n=random.randint(1,10)
-print(n)
-k=n
-attempt=5
-for i in range(5):
-    a=int(input("enter the number:-"))
+import random #importing The Random Module
+n=random.randint(1,10) #it is a function for creating a random number between ranges
+
+print(n) #you can check the generated random number by this print function
+k=n #this variable will hold the generated random number
+attempt=5 #this will use how many attempts you have left
+for i in range(5): #this loop will hold how many chances you wil give
+    a=int(input("enter the number:-")) #loop will execute
     if k==a:
         print("you won")
-        break
+        break          #if user won this loop will be break
     else:
         print("Try Again!",(attempt-1),"Left")
-        attempt=attempt-1
+        attempt=attempt-1 #this is for how many attempts you have left
 
